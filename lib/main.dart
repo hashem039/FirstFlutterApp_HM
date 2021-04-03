c8df6e2825f4594885d24660110a5f47f3a119ff
Hashem <hm.engineer039@gmail.com>
2021-04-03 22:08:39 +0530

    Second part (final)

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
     //final wordPair = WordPair.random();
     return MaterialApp(
       //title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
       home:RandomWords(),
 /*      title: 'Welcome to Flutter',
       home: Scaffold(
@@ -38,6 +41,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions(){
@@ -53,17 +57,60 @@ class _RandomWordsState extends State<RandomWords> {
     });
   }
   Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title:Text(pair.asPascalCase,style: _biggerFont)
+      title:Text(pair.asPascalCase,style: _biggerFont),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border, color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
 
   }
-
+  void _pushSaved(){
+     Navigator.of(context).push(
+       MaterialPageRoute<void>(
+         builder: (BuildContext context){
+           final tiles = _saved.map(
+               (WordPair pair) {
+                 return ListTile(
+                   title: Text(
+                     pair.asPascalCase,
+                     style: _biggerFont,
+                   ),
+                 );
+               },
+           );
+           final divided = ListTile.divideTiles(
+             context: context,
+             tiles: tiles,
+           ).toList();
+           return Scaffold(
+             appBar: AppBar(
+               title: Text('Saved Suggestions'),
+             ),
+             body:ListView(children: divided),
+           );
+         },
+       ),
+     );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hashem's first Flutter app"),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body:_buildSuggestions(),
     );
